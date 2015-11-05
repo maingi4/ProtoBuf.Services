17c67364de2270851b3c080ca970518646150b71
Sumit Maingi <maingi4@yahoo.co.in>
2015-11-05 16:57:39 +0530

    changed file versions, cleaned projects

--- ProtoBuf.Services.Serialization/Properties/AssemblyInfo.cs
+++ ProtoBuf.Services.Serialization/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ProtoBuf.Services.Serialization")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sumit Maingi")]
 [assembly: AssemblyProduct("ProtoBuf.Services.Serialization")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("http://www.apache.org/licenses/LICENSE-2.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
 [assembly: InternalsVisibleTo("ProtoBuf.Services.Wcf")]
 [assembly: InternalsVisibleTo("ProtoBuf.Services.WebAPI")]
 [assembly: InternalsVisibleTo("ProtoBuf.Services.WebAPI.Client")]
--- ProtoBuf.Services.WebAPI.Client/Properties/AssemblyInfo.cs
+++ ProtoBuf.Services.WebAPI.Client/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ProtoBuf.Services.WebAPI.Client")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Sumit Maingi")]
 [assembly: AssemblyProduct("ProtoBuf.Services.WebAPI.Client")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("http://www.apache.org/licenses/LICENSE-2.0")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
